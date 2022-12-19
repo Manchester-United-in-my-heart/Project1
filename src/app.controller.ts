35303b82714bdec1e012884e81d7cd5eb6a37b22
Manchester-United-in-my-heart <khambui2003@gmail.com>
2022-12-19 13:59:14 +0700

    m

---
+++ src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { AppService } from './app.service';
+import { Element, Query } from "./ctn/ModulesFile";
+import { query } from "express";
+import { type } from "os";
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+}
